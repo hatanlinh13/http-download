8cbd2e2e5814f7713072668991bca9db5d2d5557
hatanlinh13 <hatanlinh13@gmail.com>
2017-11-11 11:14:20 +0700

    Added comment for gobject.h

--- include/gobject.h
+++ include/gobject.h
@@ -18,6 +18,22 @@
 #ifndef GOBJECT_H__
 #define GOBJECT_H__
 
+/*
+ * Get the object at *target_location* from *host_name*.
+ * Check whether the object is a directory or a file.
+ *
+ * If this is a file then save to *curr_dir* and return.
+ *
+ * Or if this is a directory, create a directory at *curr_dir*,
+ * then set *curr_dir* point to this new directory,
+ * then recursively call *get_http_object* for all child objects,
+ * finally reset *curr_dir* to its origin value.
+ *
+ * Files and directories name form: *prefix* + name.
+ * Remember to reset *prefix* into empty
+ * at the end of first call to *get_http_object*
+ * (Sub directories and files do not have prefix).
+ */
 void get_http_object(char *host_name, char *target_location, char *curr_dir);
 
 #endif
