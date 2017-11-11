62cbbc13356b24df22fda1abb00ec2cb109c9365
hatanlinh13 <hatanlinh13@gmail.com>
2017-11-11 11:37:41 +0700

    Update htmlp.h

--- include/htmlp.h
+++ include/htmlp.h
@@ -17,4 +17,19 @@
 #ifndef HTMLP_H__
 #define HTMLP_H__
 
+/*
+ * Parsing a HTML file to check wether
+ * this file is a HTTP directory listing or not.
+ *
+ * If this file is a directory listing,
+ * add all child files and directory to *object_list*
+ * (*object_list* is a null-terminated list of C string
+ * where each entry contain target location of a child object)
+ * and return 1.
+ *
+ * Otherwise (this file is not a directory listing or cannot parse)
+ * return 0.
+ */
+int html_parser(char *data, char **object_list);
+
 #endif
